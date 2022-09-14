dd42f5b3d92787d12c25fec3563d86296c9caabf
Adevilson-Diesel <adevilson_delima@hotmail.com>
2022-09-14 14:23:44 -0300

    pasta loops while

---
+++ projetos curso em video/loops repeticões mdE/loop while e do while/ambiente.js
@@ -0,0 +1,30 @@
+
+
+//  CONDIÇÀO PARA CONTAGEM ATÉ 6 COM WHILE SIMPLES
+//C= A CONTADOR 
+var c = 1
+while(c <= 6) {
+    console.log("Tudo bem?")
+    c++  //c = c + 1  SIMPLIFICADO 1+1
+}
+
+//  CONTADOR PACONTAGEM ATÉ 10 COM INTERPOLAÇÀO DO PLACEHOLDER `${WHILE}`
+var c = 1   //INICIA COM 1 
+while(c <= 10) { //VAI ATÉ 10
+    console.log(`Passo ${c}`) //ADD NÚMEROS A RESPOSTA 
+    c++
+}
+
+//ESTRUTURA DE REPETIÇÀO COM TESTE LÓGICO NO FINAL DEPOIS DO BLOCO  "DO{}WHILE" SEMELHANTE AO WHILE USE A QUE ACHAR MELHOR 
+//x = CONTADOR
+var C = 1
+do {
+    console.log(`PassoX ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+//OS 3 ESTÃO FUNCIONANDO 
+//while; `${while}`; do{}while
+
