aa7f8272fbda9111f51c1a6b4b9c53904958850a
Adevilson-Diesel <adevilson_delima@hotmail.com>
2022-09-15 11:20:07 -0300

    loops com for

---
+++ projetos curso em video/javaScript cV/loops for/script.js
@@ -0,0 +1,14 @@
+//alert("1")
+
+
+//LOOP DE 1 A 5 COM while
+var c  = 1    //COMEÇA COM 1
+while (c <=5) { //EQUANTO c FOR MENOR QUE 5 SEGUE O LOOP
+    console.log(c) //MOSTRA O RESULTADO 
+    c++            //INCREMENENTA +1 COM O RESULTADO E INICIA O TESTE ATÉ CHEGAR A 5    
+}
+
+//LOOP DE 1 A 5 COM for
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
