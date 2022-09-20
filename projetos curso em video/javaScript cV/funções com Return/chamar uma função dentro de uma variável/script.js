ccd814064c59458095a466e931fc977f0016f032
Adevilson-Diesel <adevilson_delima@hotmail.com>
2022-09-20 10:02:34 -0300

    add pasta fincao dentro de uma variavel

---
+++ projetos curso em video/javaScript cV/funções com Return/chamar uma função dentro de uma variável/script.js
@@ -0,0 +1,7 @@
+//COLOCAR UMA FUNÇÃO DENTRO DE UMA VARIÁVEL
+let v = function(x) {
+    return x*2  // x vezes 2 
+}
+console.log(v(5)) //v recebe 5 como parâmetro
+
+//dica: existe a função flexa "arrow function" => 
