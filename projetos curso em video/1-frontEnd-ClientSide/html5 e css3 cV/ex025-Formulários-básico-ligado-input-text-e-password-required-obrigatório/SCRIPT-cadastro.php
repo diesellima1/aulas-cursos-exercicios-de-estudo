d188b95cc399db3fe88e1752b8815e033193c365
Adevilson-Diesel <adevilson_delima@hotmail.com>
2022-10-24 08:19:04 -0300

    ex025-elementos-input-form

---
+++ projetos curso em video/1-frontEnd-ClientSide/html5 e css3 cV/ex025-Formulários-básico-ligado-input-text-e-password-required-obrigatório/SCRIPT-cadastro.php
@@ -0,0 +1,8 @@
+Os dados foram enviados!
+Aprenda PHP para saber o que fazer com eles. 
+
+<!-- mensagem para não aparecer erro depois de enviar os dados  -->
+
+<!-- renomear com o mesmo nome usado após action
+
+<form action="SCRIPT-cadastro.php" method="post" autocomplete="off">-->
